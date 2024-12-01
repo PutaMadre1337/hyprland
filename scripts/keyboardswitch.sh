54fe62978f00425f08f72b1e04fa3106c78e9844
a <a>
2024-12-01 12:55:06 +0300

    1

---
+++ scripts/keyboardswitch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+scrDir=`dirname "$(realpath "$0")"`
+source $scrDir/globalcontrol.sh
+
+# hyprctl switchxkblayout all next
+
+hyprctl switchxkblayout by-tech-gaming-keyboard next
+layMain=$(hyprctl -j devices | jq '.keyboards' | jq '.[] | select (.main == true)' | awk -F '"' '{if ($2=="active_keymap") print $4}')
+notify-send -a "t1" -r 91190 -t 800 -i "~/.config/dunst/icons/keyboard.svg" "${layMain}"
+
